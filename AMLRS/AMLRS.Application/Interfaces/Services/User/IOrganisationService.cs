6bff5493e3e3d7d97854c134296c1772214437ef
GaneshMore <GaneshMore@DESKTOP-RP84GB3>
2026-02-05 19:39:01 +0530

    orgnisation filter add in main

--- AMLRS/AMLRS.Application/Interfaces/Services/User/IOrganisationService.cs
+++ AMLRS/AMLRS.Application/Interfaces/Services/User/IOrganisationService.cs
@@ -1,10 +1,11 @@
 ﻿using AMLRS.Application.DTOs;
+using AMLRS.Core.QueryModels;
 
 namespace AMLRS.Application.Interfaces.Services.User
 {
     public interface IOrganisationService
-    {     
-        Task<IEnumerable<OrganisationResponseDto>> GetAllOrganisationsAsync();
+    {
+        Task<PagedResult<OrganisationResponseDto>> GetAllOrganisationsAsync(OrgQueryParams queryParams);
         Task<OrganisationResponseDto?> GetOrganisationByIdAsync(int orgId);
         Task<OrganisationResponseDto> AddOrganisationAsync(OrganisationDto organisationDto);
         Task<OrganisationDto> UpdateOrgAsync(int id, OrganisationDto orgDto);
--- AMLRS/AMLRS.Core/Abstraction/Reposotory/User/IOrganisationRepository.cs
+++ AMLRS/AMLRS.Core/Abstraction/Reposotory/User/IOrganisationRepository.cs
@@ -1,9 +1,12 @@
 ﻿using AMLRS.Core.Domains.OrganisationAdmins.Entites;
+using AMLRS.Infrastructure.Data;
 
 namespace AMLRS.Core.Abstraction.Reposotory.User
 {
     public interface IOrganisationRepository : IRepository<Organisation, int>
     {
         Task<Organisation?> GetOrganisationByOrgNameAsync(string name);
+        IQueryable<Organisation?> GetAllOrganisationQueryable();
+
     }
 }
